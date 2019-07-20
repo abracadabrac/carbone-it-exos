861536cd4fa5ee059cda5cfaa9ca075952f4f46e
R2D2 <charles@r2d2s-air.home>
2019-07-20 10:20:22 +0200

    factor(read_input_file) change elementsDict data strucure

--- read_input_file.py
+++ read_input_file.py
@@ -2,18 +2,21 @@ def read_input_file(input_file_path):
 	"""
 	this function reads the input_file and returs a dictionary in the fallowing format
 	{
-	"C": [[{width's map}, {lenght's map}]],
-	"M": [[{y of mountain 0}, {x of mountain 1}], ..., [{y of mountain i}, {x of mountain i}]],
-	"T": [[{nb fo treasures 0}, {y of treasures 0}, {x of treasures 1}], ..., 
-		  [{nb fo treasures i}, {y of treasures i}, {x of treasures i}]]
-	"A": []
+	"C": [{<width's map>}, {<lenght's map>}],
+	"M": [[<y0>, <x1>], ..., [<yi>, <xi>]],
+	"T": [[<y0>, <x1>], ..., [<yi>, <xi>]], 	# coords are duplicated if multiple treasures are present on it
+	"A": [{	'name': <name>, 
+			'coords': [<y>, <x>], 
+			'orientation': <N or W or E or S>, 
+			'moves': <ex: 'AADADAGGA'>
+		  }]
 	}
 	"""
 	with open(input_file_path, "r") as f: inputData = f.read()
-	# this function build a Dict of all elements of the game
 	lines = inputData.split("\n")
-	#print("lines\n", lines)
+
 	elementsDict = {"C": [], "M": [], "T": [], "A": []}
+
 	for line in lines:
 		lineSplit = line.split("\u200b")
 		elemType = lineSplit[0]
@@ -21,12 +24,22 @@ def read_input_file(input_file_path):
 			elemData = lineSplit[1].split(" - ")[1:]
 			elemData = [int(e) for e in elemData]
 			if elemType == "C":
-				elementsDict[elemType] = elemData
-			else:
-				elementsDict[elemType].append(elemData)
+				elementsDict["C"] = elemData
+			elif elemType == "M":
+				elementsDict["M"].append(elemData)
+			elif  elemType == "T":
+				for treasure in range(elemData[2]):
+					elementsDict["T"].append(elemData[:2])
+
 		elif elemType == "A":
 			elemData = lineSplit[1].split("-")[1:]
-			elementsDict[elemType].append(elemData)
+			elementsDict[elemType].append({
+					"name": elemData[0],
+					"coords": elemData[1:3],
+					"orientation": elemData[3],
+					"moves": elemData[4]
+				}
+			)
 
 	return elementsDict
 
