9eae1ebd67ed568f1920ae199b3eb6ecd8ddffe9
R2D2 <charles@r2d2s-air.home>
2019-07-20 10:47:54 +0200

    factor(map) use property to build the map instead of a function

--- map.py
+++ map.py
@@ -1,6 +1,7 @@
 import numpy as np
 from read_input_file import read_input_file
 
+
 class Map:
 	"""this class represent the map"""
 	def __init__(self, input_file_path):
@@ -13,27 +14,28 @@ class Map:
 		self.dimensions = elements["C"]
 		self.mountainsList = elements["M"]
 		self.TreasuresList = elements["T"]
-		self.build_mapArray()
 
-
-	def build_mapArray(self):
+	@property
+	def _mapArray(self):
 		"""build the array's map representation into self.mapArray"""
-		self.mapArray = np.chararray((self.dimensions[1], self.dimensions[0]), unicode=True)
-		self.mapArray[:] = '.'
+		mapArray = np.chararray((self.dimensions[1], self.dimensions[0]), unicode=True)
+		mapArray[:] = '.'
 
 		for mountainCoord in self.mountainsList:
-			self.mapArray[mountainCoord[1], mountainCoord[0]] = "M"
+			mapArray[mountainCoord[1], mountainCoord[0]] = "M"
 
 		for treasureCoord in self.TreasuresList:
-			if self.mapArray[treasureCoord[1], treasureCoord[0]] == '.':
-				self.mapArray[treasureCoord[1], treasureCoord[0]] = 1
+			if mapArray[treasureCoord[1], treasureCoord[0]] == '.':
+				mapArray[treasureCoord[1], treasureCoord[0]] = 1
 			else: 
-				self.mapArray[treasureCoord[1], treasureCoord[0]] =\
-				 int(self.mapArray[treasureCoord[1], treasureCoord[0]]) + 1
+				mapArray[treasureCoord[1], treasureCoord[0]] =\
+				 int(mapArray[treasureCoord[1], treasureCoord[0]]) + 1
+
+		return mapArray
 
 
 
 if __name__ == "__main__":
 	input_file_path = "/Users/charles/Workspace/carbone-it-exos/entry_file.txt"
 	map = Map(input_file_path)
-	print(map.mapArray)
+	print(map._mapArray)
